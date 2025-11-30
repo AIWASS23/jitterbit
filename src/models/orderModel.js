b391fa8b9f5e600678873953d982d1eda7a0a996
AIWASS23 <marcelodearaujo677@gmail.com>
2025-11-30 18:22:13 -0300

    feat/create models

---
+++ src/models/orderModel.js
@@ -0,0 +1,82 @@
+const { DataTypes, Model } = require('sequelize');
+const { sequelize } = require('../database/db');
+
+class Order extends Model {}
+Order.init(
+  {
+    orderId: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      allowNull: false,
+    },
+    value: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    creationDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Order',
+    tableName: 'orders',
+    timestamps: true, // createdAt / updatedAt
+  }
+);
+
+/* ---------- Model Item ---------- */
+class Item extends Model {}
+Item.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    productId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    // FK para Order
+    orderId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: 'orders',
+        key: 'orderId',
+      },
+      onDelete: 'CASCADE',
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Item',
+    tableName: 'items',
+    timestamps: false,
+  }
+);
+
+// Um Order tem vários Items ( Associações )
+Order.hasMany(Item, {
+  foreignKey: 'orderId',
+  sourceKey: 'orderId',
+  as: 'items',
+  onDelete: 'CASCADE',
+});
+Item.belongsTo(Order, {
+  foreignKey: 'orderId',
+  targetKey: 'orderId',
+  as: 'order',
+});
+
+module.exports = { Order, Item };
---
+++ src/models/userModel.js
@@ -0,0 +1,31 @@
+// src/models/user.model.js
+const { DataTypes, Model } = require('sequelize');
+const { sequelize } = require('../database/db');
+
+class User extends Model {}
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    passwordHash: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'User',
+    tableName: 'users',
+    timestamps: true,
+  }
+);
+
+module.exports = User;
